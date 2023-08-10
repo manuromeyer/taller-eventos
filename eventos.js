6dbee141d6a1e1c610523f25ea110a742760cf4e
manuromeyer <manurodriguez2015.11@gmail.com>
2023-08-10 12:06:28 -0300

    div funcionando

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
 function mensaje(){
     alert('Hola!');
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    var divElement = document.getElementById('alerta');
+    divElement.addEventListener('click', function() {
+        alert("Hola! Soy el div");
+    });
+})
